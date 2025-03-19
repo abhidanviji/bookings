21a9462a2abd6c7c5d6cecb0d80e27f7a4415d94
Abhinaya Dhandapani <abhinaya_dhandapani@selinc.com>
2025-03-19 11:21:20 -0700

    Initialize server form validations

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/abhidanviji/bookings/internal/config"
+	"github.com/abhidanviji/bookings/internal/forms"
 	"github.com/abhidanviji/bookings/internal/models"
 	"github.com/abhidanviji/bookings/internal/render"
 )
@@ -54,7 +55,14 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Reservation renders the make a reservation page and displays form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+		Form: forms.New(nil),
+	})
+}
+
+// PostReservation handles the posting of a reservation form
+func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+
 }
 
 // Genarals renders the room page
